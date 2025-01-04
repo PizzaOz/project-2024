acba0f1df2007e64e1e152ed36197b87ae5f179a
Mikhail Kos <him182@yandex.ru>
2025-01-04 15:17:57 +0300

    dialog

--- my-app/src/component/Dialog/Dialog.jsx
+++ my-app/src/component/Dialog/Dialog.jsx
@@ -1,9 +1,33 @@
 import React from "react";
+import s from './Dialog.module.css'
+
+console.log(s.dialogs)
 
 const Dialog = (props) => {
     return (
-        <div>
-            Dialog
+        <div className={s.dialogs}>
+        <div className={s.dialogsItems}>
+            <div className={`${s.dialog} ${s.active}`}>
+                Misha
+            </div>
+            <div className={s.dialog}>
+                Vlada
+            </div>
+            <div className={s.dialog}>
+                Dima
+            </div>
+            <div className={s.dialog}>
+                Sergey
+            </div>
+            <div className={s.dialog}>
+                Vladimer
+            </div>
+        </div>
+        <div className={s.messages}>
+            <div className={s.dialog}>Hi</div>
+            <div className={s.dialog}>How is it</div>
+            <div className={s.dialog}>Yo</div>
+        </div>
         </div>
     )
 }

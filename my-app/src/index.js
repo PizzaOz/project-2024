44414f973bf97373c1b9c89a67c8a5bf606711e0
Mikhail Kos <him182@yandex.ru>
2025-01-09 23:57:21 +0300

    store

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -3,8 +3,8 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import state from './state/state';
-import { addPost, updateNewPostText, subscribe} from './state/state';
+import store from './state/state';
+
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
@@ -12,13 +12,13 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 const rerenderEntireTree = (state) => {
   root.render(
     <React.StrictMode>
-      <App state={state} addPost={addPost} updateNewPostText={updateNewPostText}/>
+      <App state={state} addPost={store.addPost.bind(store)} updateNewPostText={store.updateNewPostText.bind(store)}/>
     </React.StrictMode>
   );
 }
 
-rerenderEntireTree(state)
-subscribe(rerenderEntireTree)
+rerenderEntireTree(store.getState())
+store.subscribe(rerenderEntireTree)
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
--- my-app/src/state/state.js
+++ my-app/src/state/state.js
@@ -1,64 +1,63 @@
 
-let rerenderEntireTree = () => {
-    console.log('q')
-}
 
-const state = {
-    dialogPage: {
-        dialog: [
-            { id: '1', name: 'Misha' },
-            { id: '2', name: 'Vlada' },
-            { id: '3', name: 'Dima' },
-            { id: '4', name: 'Sergey' },
-            { id: '5', name: 'Vladimer' },
-        ],
-        message: [
-            { id: '1', message: 'Hi' },
-            { id: '2', message: 'How is it ? ' },
-            { id: '3', message: 'Yo' },
-            { id: '4', message: 'Boo' },
-            { id: '5', message: 'Privet' },
-        ],
+const store = {
+    _state: {
+        dialogPage: {
+            dialog: [
+                { id: '1', name: 'Misha' },
+                { id: '2', name: 'Vlada' },
+                { id: '3', name: 'Dima' },
+                { id: '4', name: 'Sergey' },
+                { id: '5', name: 'Vladimer' },
+            ],
+            message: [
+                { id: '1', message: 'Hi' },
+                { id: '2', message: 'How is it ? ' },
+                { id: '3', message: 'Yo' },
+                { id: '4', message: 'Boo' },
+                { id: '5', message: 'Privet' },
+            ],
+        },
+        profilePage: {
+            post: [
+                { id: 1, message: 'hi ssss', likeCount: 12 },
+                { id: 2, message: 'hi You', likeCount: 14 },
+                { id: 3, message: 'hi eee', likeCount: 17 },
+                { id: 4, message: 'his eee', likeCount: 3 },
+            ],
+            newPostText: 'hellou text'
+        },
+        friendsPage: {
+            friends: [
+                { friend: 'Sergey', img: "https://static.insales-cdn.com/images/products/1/1979/515671995/large_kotiki_kolpaki_kotiki_fotografichnye_6_shtuk_1501_4985.jpg" },
+                { friend: 'Anton', img: "https://711515.selcdn.ru/recycleCDN/main/9/9ead29f7ebb448d64cd47e567738b77a_thumbnail.jpg" },
+                { friend: 'Dima', img: "https://ybis.ru/wp-content/uploads/2023/09/kotiki-s-serdechkami-12.webp" },
+            ]
+        }
     },
-    profilePage: {
-        post: [
-            { id: 1, message: 'hi ssss', likeCount: 12 },
-            { id: 2, message: 'hi You', likeCount: 14 },
-            { id: 3, message: 'hi eee', likeCount: 17 },
-            { id: 4, message: 'his eee', likeCount: 3 },
-        ],
-        newPostText: 'hellou text'
+    getState() {
+        return this._state;
     },
-    friendsPage: {
-        friends: [
-            { friend: 'Sergey', img: "https://static.insales-cdn.com/images/products/1/1979/515671995/large_kotiki_kolpaki_kotiki_fotografichnye_6_shtuk_1501_4985.jpg" },
-            { friend: 'Anton', img: "https://711515.selcdn.ru/recycleCDN/main/9/9ead29f7ebb448d64cd47e567738b77a_thumbnail.jpg" },
-            { friend: 'Dima', img: "https://ybis.ru/wp-content/uploads/2023/09/kotiki-s-serdechkami-12.webp" },
-        ]
+    _rerenderEntireTree(){
+        console.log('hi')
+    },
+    addPost() {
+        const newPost = {
+            id: 5,
+            message: this._state.profilePage.newPostText,
+            likeCount: 0,
+        };
+        this._state.profilePage.post.push(newPost)
+        this._state.profilePage.newPostText = ''
+        this._rerenderEntireTree(this._state)
+    },
+    updateNewPostText(newText) {
+        this._state.profilePage.newPostText = newText;
+        this._rerenderEntireTree(this._state);
+    },
+    subscribe(observer) {
+        this._rerenderEntireTree = observer
     }
 }
 
-
-export const addPost = () => {
-    const newPost = {
-        id: 5,
-        message: state.profilePage.newPostText,
-        likeCount: 0,
-    };
-    state.profilePage.post.push(newPost)
-    state.profilePage.newPostText = ''
-    rerenderEntireTree(state)
-}
-
-export const updateNewPostText = (newText) => {
-
-    state.profilePage.newPostText = newText;
-    rerenderEntireTree(state);
-}
-
-
-export const subscribe = (observer) => {
-    rerenderEntireTree = observer
-}
-
-export default state
+export default store
